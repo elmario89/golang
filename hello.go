85e852f7bcbb58a6879af7d2d3169516676f50a7
Mikhail Smirnov <urine89@mail.ru>
2021-12-28 21:31:11 +0300

    Initial commit

---
+++ hello.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type Book struct {
+	id     int
+	name   string
+	author string
+}
+
+type Library struct {
+	books   []Book
+	address string
+}
+
+func (p Library) printAddress() {
+	fmt.Println(p.address)
+}
+
+func (b Book) printBookInfo() {
+	fmt.Println("The book name is", b.name)
+	fmt.Println("The author name is", b.author)
+}
+
+func (p Library) callBooksInfo() {
+	for i := range p.books {
+		p.books[i].printBookInfo()
+	}
+}
+
+func (p Library) printAllBooks() {
+	p.callBooksInfo()
+}
+
+func main() {
+	var testBooks = []Book{{1, "War and peace", "Lev Tolstoy"}, {2, "Blokada", "Chakovskui"}}
+	var testLibrary = Library{address: "Prospekt prosveshenia"}
+	testLibrary.books = testBooks
+
+	testLibrary.printAddress()
+	testLibrary.printAllBooks()
+}
